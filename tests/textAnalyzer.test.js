39838383aefe19a4dad6f8b5d51eca2acd2db7a9
MD2020m <dmarshall2019@gmail.com>
2025-11-02 19:18:20 -0600

    Edit textAnalyzer.test.js to match changes to textAnalyzer.js

--- tests/textAnalyzer.test.js
+++ tests/textAnalyzer.test.js
@@ -2,29 +2,24 @@ const { CountWords, LongestWord, CountLines } = require("./src/textAnalyzer");
 
 describe('Should count the number of words in any string', () => {
     test('Should count number of words in a simple string', () => {
-        result = CountWords("Hello world test");
-        expect(result).toBe(3);
+        result = CountWords("./data/sample-text.txt");
+        expect(result).toBe(207);
     });
 
     test('Should count number of words in empty string', () => {
-        result = CountWords('');
+        result = CountWords("./data/empty.txt");
         expect(result).toBe(0);
-    });
-
-    test('Should count number of words with extra spaces', () => {
-        result = CountWords('   Hello   World  Test');
-        expect(result).toBe(3);
     });
 });
 
 describe('Should find the longest word in any string', () => {
     test('Should find longest word in a simple string', () => {
-        result = LongestWord('A word is supercalifragilisticexpialidocious');
-        expect(result).toBe('supercalifragilisticexpialidocious');
+        result = LongestWord('./data/sample-text.txt');
+        expect(result).toBe('authentication');
     });
 
     test('Should find longest word in empty string', () => {
-        result = LongestWord('');
+        result = LongestWord('./data/empty.txt');
         expect(result).toBe('');
     });
 });
